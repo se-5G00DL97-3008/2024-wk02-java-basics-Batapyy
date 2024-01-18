f16c35c4a6256bb098742120d0faa31578b0ea48
Batapyy <103754577+Batapyy@users.noreply.github.com>
2024-01-18 08:56:05 +0200

    Update E08.java

--- e08/E08.java
+++ e08/E08.java
@@ -1,5 +1,15 @@
-class E08 {
-    public static void main(String[] args) {
+Use the code skeleton in the e08/E08.java file.
+Write a program that will tell a user if a positive number is odd or even, the program will keep asking a new number until the user enter 0.
 
-    }
-}
+💡Using modulo % to print if number is odd or even. Modulo returns the remainder when a division is done. 5 % 2 = 1, 4 % 2 = 0
+
+
+    // Expected flow
+    Enter a number (0 to quit):
+    9
+    Odd number
+    Enter a number (0 to quit):
+    12
+    Even number
+    Enter a number (0 to quit):
+    0

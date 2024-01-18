87efd52038d06a1c0e52a0438865670f2966ff7d
Batapyy <103754577+Batapyy@users.noreply.github.com>
2024-01-18 08:46:59 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -1,5 +1,27 @@
-class E02 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class E02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        double num1 = scanner.nextDouble();
+
+ 
+        System.out.println("Give another number:");
+        double num2 = scanner.nextDouble();
+
+      
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        
+        if (num2 != 0) {
+            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        } else {
+            System.out.println("Cannot divide by zero.");
+        }
+
+        scanner.close();
     }
 }

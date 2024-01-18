e9228746125863497b2b74cbb1b7f13d6514981e
Batapyy <103754577+Batapyy@users.noreply.github.com>
2024-01-18 09:00:01 +0200

    Update E04.java

--- e04/E04.java
+++ e04/E04.java
@@ -5,11 +5,11 @@ public class E04 {
         Scanner scanner = new Scanner(System.in);
 
         // Ask the user to enter the day
-        System.out.println("Enter a day:");
+        System.out.println("Enter a day: ");
         int day = scanner.nextInt();
 
         // Ask the user to enter the month
-        System.out.println("Enter a month:");
+        System.out.println("Enter a month: ");
         int month = scanner.nextInt();
 
         // Check if it's Christmas (day 24 and month 12)

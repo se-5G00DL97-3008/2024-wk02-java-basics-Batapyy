51f847bf61296f94f15f79571e9e3f843eea9414
Batapyy <103754577+Batapyy@users.noreply.github.com>
2024-01-18 11:46:49 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -13,8 +13,9 @@ public class E02 {
 
       
         System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
-        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
         System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+     
         
         if (num2 != 0) {
             System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));

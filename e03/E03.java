9931f99590b884a1418e6ad9a9a40be0dd382197
Batapyy <103754577+Batapyy@users.noreply.github.com>
2024-01-18 08:48:07 +0200

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -1,5 +1,22 @@
-class E03 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class E03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        System.out.println("Enter a number:");
+        double number = scanner.nextDouble();
+
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else if (number < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+
+
+        scanner.close();
     }
 }

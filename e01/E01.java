03d50943499ee51749e86c09d12292856e0af5db
Batapyy <103754577+Batapyy@users.noreply.github.com>
2024-01-18 08:44:49 +0200

    Update E01.java

--- e01/E01.java
+++ e01/E01.java
@@ -1,5 +1,23 @@
-class E01 {
+public class E01 {
     public static void main(String[] args) {
+        // Declare and initialize variables
+        int x = 10;
+        float y = 5.5f;
+        boolean flag = true;
+        char letter = 'c';
+        short year = 2022;
+        long population = 7000000000L;
+        byte num = 100;
+        double pi = 3.13457599923384753929348;
 
+        // Print the variables to the console
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(flag);
+        System.out.println(letter);
+        System.out.println(year);
+        System.out.println(population);
+        System.out.println(num);
+        System.out.println(pi);
     }
 }
